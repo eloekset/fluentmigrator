279ed516f0ee67f8ef61765d2e59324ef5afc01a
Eivind Gussiås Løkseth <eloekset@msn.com>
2018-02-20 19:28:36 +0100

    Add sample project for .NET Standard 2.0 implementation.

---
+++ src/Fluent.NetStandardTest/Migrations/TestMigration.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using System;
+
+namespace Fluent.NetStandardTest.Migrations
+{
+    [Migration(1)]
+    public class TestMigration : Migration
+    {
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Up()
+        {
+            if (!Schema.Table("TestTable").Exists())
+            {
+                Create.Table("TestTable")
+                    .WithColumn("Id").AsGuid()
+                    .WithColumn("Name").AsString(200);
+            }
+        }
+    }
+}
---
+++ src/Fluent.NetStandardTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+using System;
+using System.Reflection;
+
+namespace Fluent.NetStandardTest
+{
+    class Program
+    {
+        private static readonly ConsoleAnnouncer consoleAnnouncer = new ConsoleAnnouncer();
+        private static RunnerContext RunnerContext { get; set; }
+
+        static void Main(string[] args)
+        {
+            consoleAnnouncer.Header();
+            consoleAnnouncer.Emphasize("Attach debugger now and press any key to execute migrations...");
+            Console.ReadKey();
+            
+            ExecuteMigrations(consoleAnnouncer);
+
+
+            Console.ReadKey();
+        }
+
+        private static void ExecuteMigrations(IAnnouncer announcer)
+        {
+            RunnerContext = new RunnerContext(announcer)
+            {
+                Database = "SQLServer", //SQLite",
+                Connection = @"Data Source=(local)\SQL2016;Initial Catalog=TestFluent;Integrated Security=true", //@"Data Source=.\dbfile.db",
+                Targets = new string[] { Assembly.GetExecutingAssembly().FullName },
+                Task = "migrate:up",
+            };
+
+            new TaskExecutor(RunnerContext).Execute();
+        }
+    }
+}
