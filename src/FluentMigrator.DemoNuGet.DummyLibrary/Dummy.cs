fbc48134605b40ea7df95ea10f39ecbfbbba7949
Eivind Gussiås Løkseth <eloekset@msn.com>
2019-12-24 14:05:13 +0100

    Dummy projects to test views of NuGet feed.

---
+++ src/FluentMigrator.DemoNuGet.DummyLibrary/Dummy.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace FluentMigrator.DemoNuGet.DummyLibrary
+{
+    public class Dummy
+    {
+        public static Version Version
+        {
+            get { return typeof(Dummy).Assembly.GetName().Version; }
+        }
+        public static string Description
+        {
+            get
+            {
+                return $"The alpha of version {Version}";
+            }
+        }
+    }
+}
---
+++ src/FluentMigrator.DemoNuGet.TestApp/Program.cs
@@ -0,0 +1,15 @@
+using System;
+//using FluentMigrator.DemoNuGet.DummyLibrary;
+
+namespace FluentMigrator.DemoNuGet.TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Call DummyLibrary to display its version...");
+            //Console.WriteLine(Dummy.Description);
+            Console.ReadKey();
+        }
+    }
+}
